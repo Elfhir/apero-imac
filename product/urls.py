ef93b1daea213948ed70eb695b20e98598b95c37
Elfhir <ta.thanhliem@gmail.com>
2013-11-04 00:20:08 +0100

    Begins review, product, do some settings and scss files

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from product.views import *
+
+"""
+urlpatterns = patterns('product.views',
+	url(r'^$', ''),
+)
+"""
---
+++ review/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from review.views import *
+
+"""
+urlpatterns = patterns('review.views',
+	url(r'^$', ''),
+)
+"""
